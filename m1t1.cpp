72867dab66a0a38d10554ef74b88f19757787710
josiah-annoh <josiahannoh915@gmail.com>
2026-01-12 18:59:53 +0000

    Started M1T1

---
+++ m1t1.cpp
@@ -0,0 +1,13 @@
+// csc 134 
+// M1T1 (Module 1, tutorial 1)
+
+//we'll explain this later
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Say hello
+    cout << "Hello World!" << endl;
+    cout << endl; // blank line
+    return 0; //exit with no errors
+}
