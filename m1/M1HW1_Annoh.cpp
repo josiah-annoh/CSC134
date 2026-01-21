e7a721e4c1fe3e4136fa80e37ae490a3f931c839
josiah-annoh <josiahannoh915@gmail.com>
2026-01-21 19:44:09 +0000

    Started M1HW1

---
+++ m1/M1HW1_Annoh.cpp
@@ -0,0 +1,19 @@
+//CSC 134
+// M1HW1 Movie Talk
+// Josiah Annoh
+// 1/21/26
+
+
+#include <iostream>
+# include <iomanip>
+using namespace std;
+
+int main () {
+string item_name = Zootopia 2;
+int item_count = 2025;
+double price_each = 1.69;
+
+cout << "The movie" << Zootopia 2 << endl;
+
+
+}
