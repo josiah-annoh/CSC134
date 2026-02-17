5b2b6991b34750eb1a23672eeb26d13dab08ae74
josiah-annoh <josiahannoh915@gmail.com>
2026-02-17 19:18:50 +0000

    declared variables

---
+++ m3t1_Annoh.cpp
@@ -0,0 +1,12 @@
+/*
+m3t1
+compare two triangles
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+      // Program:measure two triangles
+      //declare variables
+      double length1, width1, area1;
+      double length2, width2, area2;
