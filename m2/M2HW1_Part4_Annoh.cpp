0f124668b866ae8a4781dd75544703a8ab3cd375
josiah-annoh <josiahannoh915@gmail.com>
2026-02-20 00:56:23 +0000

    Finished Part 4 of M2HW1

---
+++ m2/M2HW1_Part4_Annoh.cpp
@@ -0,0 +1,34 @@
+/*
+CSC 134
+M2HW1 Part 4 - Gold
+Josiah Annoh
+2/19/2026
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main () {
+
+    string letsGo, school, team, cheerOne, cheerTwo;
+
+    letsGo = "Let's go";
+    school = "FTCC";
+    team = "Trojans";
+
+    cheerOne = letsGo + " " + school;
+    cheerTwo = letsGo + " " + team;
+
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+
+    cout << cheerTwo << endl;
+
+
+
+
+
+}
