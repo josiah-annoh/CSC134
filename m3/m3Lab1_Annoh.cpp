a0dcd00288c62be0ab7c4d3f53ffe051ebdbe940
josiah-annoh <josiahannoh915@gmail.com>
2026-02-25 19:28:35 +0000

    Finished first version of m3lab1

--- m3/m3Lab1_Annoh.cpp
+++ m3/m3Lab1_Annoh.cpp
@@ -2,8 +2,6 @@
 
 #include <iostream>
 
-
-
 using namespace std;
 
 // List PROTOTYPES of all the choices up here
@@ -12,6 +10,10 @@ void Fire();
 void Lightning();
 void Air();
 void Water();
+void start_battle();
+
+string elemental_name;
+
 
 // main() goes here
 int main() {
@@ -45,32 +47,56 @@ int main() {
     else {
         cout << "I didn't understand that." << endl;
     }
-    cout << "*GAME OVER*" << endl << endl;
+    
+    cout << " *GAME OVER* " << endl << endl;
 
     return 0; // end of game
 }
 
 // List FULL FUNCTIONS of all the choices at the bottom
 void Fire() {
+    elemental_name = "Fire";
     cout << "You chose Fire." << endl;
-    cout << "And then... you spawn right next to Giant Elemental Monster" << endl; // you finish it!
-    cout << "1. Another Choice" << endl;
-    cout << "2. Still another choice" << endl;
-    int choice;
-    cin >> choice;
+    cout << "And then... you spawn in a random den" << endl; // you finish it!
+    start_battle();
 }
 
+
 void Lightning() {
+    elemental_name = "Lightning";
     cout << "You chose Lightning." << endl;
-    cout << "And then... you spawn right next to Giant Elemental Monster"; // you finish it!
+    cout << "And then... you spawn in a random den";
+    start_battle();
 }
 
 void Air() {
+    elemental_name = "Air";
     cout << "You chose Air." << endl;
-    cout << "And then... you spawn right next to Giant Elemental Monster"; // you finish it!
+    cout << "And then... you spawn in a random den" << endl;
+    start_battle();
 }
 
 void Water() {
+    elemental_name = "Water";
     cout << "You chose Water." << endl;
-    cout << "And then... you spawn right next to Giant Elemental Monster"; // you finish it!
+    cout << "And then... you spawn in a random den" << endl;
+    start_battle();
+}
+
+void start_battle() {
+    cout << "A Giant Elemental Zombie appears!" << endl;
+    cout << "What do you do?" << endl;
+    cout << "1. Shoot a " << elemental_name << " blast" << endl;
+    cout << "2. Run away" << endl;
+
+    int choice;
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "You unleash your " << elemental_name << " power!" << endl;
+        cout << "The zombie is defeated!" << endl;
+    } else if (choice == 2) {
+        cout << "You run away scared..." << endl;
+        cout << "*GAME OVER*" << endl;
+    }
 }
